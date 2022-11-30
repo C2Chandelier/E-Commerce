f34e246ce635ad0a54fdcdf57676dfead466c1ae
C2Chandelier <charlelie.chandelier@epitech.eu>
2022-11-30 12:48:39 +0100

    wallah

--- back/src/Entity/Articles.php
+++ back/src/Entity/Articles.php
@@ -23,6 +23,12 @@ class Articles
     #[ORM\Column(length: 255)]
     private ?string $description = null;
 
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $titre = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -48,6 +54,30 @@ class Articles
     public function setDescription(string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
 
         return $this;
     }
