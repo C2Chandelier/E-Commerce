af285a8be50a0456a64bfbdb9bbc7df528f7d3eb
Nathan7594 <nathan.75@outlook.com>
2022-12-09 16:04:04 +0100

    Navbar fonctionnel Nathan*

--- front/src/component/Navbar/ Navbar.js
+++ front/src/component/Navbar/ Navbar.js
@@ -3,16 +3,15 @@ import React from 'react'
 import './Navbar.css'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Card_collection from '../card_collection/card_collection'
 
 import BarreRecherche from '../Barre/barre'
 
 export default function Navbar() {
 
-  const [error, setError] = useState(null);
-  const [categorie, setCategorie] = useState([]);
-  const [categorieId] = useState([]);
-  const [SousCategorie, setSousCategorie] = useState([]);
-  const [SouscategorieId] = useState([]);
+  const [error, setError] = useState(false);
+  const [categorie, setCategorie] = useState(null);
+
 
 
 
@@ -25,95 +24,30 @@ export default function Navbar() {
       })
       .catch(setError);
 
-      axios("https://localhost:8000/api/souscategories")
-      .then((res) => {
-        setSousCategorie(res.data["hydra:member"])
-        setError(null);
-      })
-      .catch(setError);
-
   }, []);
   if (error) return <p>An error occurred</p>
 
   return (
     <nav className="Navbar">
-    {categorie.map((item) => (
-      categorieId.push(item.id)
-    ))};
-    {SousCategorie.map((item) => (
-      SouscategorieId.push(item.id)
-    ))};
+     {categorie ?
+    
       <ul className="liste">
         <div className="divlogo1">
           <Link to={"/"}><img src="/images/Image_Navbar/logo.png" className="logo1" alt="costume" /></Link>
         </div>
-        <li><Link to={"/result/categorie/" + categorieId[0]}>Costumes</Link></li>
-        <li><Link to={"/result/categorie/" + categorieId[1]}>Chemises</Link></li>
-        <li><Link to={"/result/categorie/" + categorieId[2]}>Vêtements</Link></li>
+        <li><Link to={"/result/categorie/" + categorie[0].id}>Costumes</Link></li>
+        <li><Link to={"/result/categorie/" + categorie[1].id}>Chemises</Link></li>
 
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[0]}>cravate</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[1]}>chaussures</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[2]}>ceinture</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[3]}>pantalon</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[4]}>gilet</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[5]}>Veste</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[6]}>pull</Link></li>
+        <li className="items" id="collection_btn">Collection <img className='fleche' src='/images/Image_Navbar/fleche-removebg-preview.png'></img><Card_collection /></li> 
+
 
         <li><Link to={"/"}><img className="logo2" src="/images/Image_Navbar/ajouter-au-panier.png" alt="costume" /></Link></li>
         <li><Link to={"/login"}><img className="logo2" src="/images/Image_Navbar/logoprofil.png" alt="costume" /></Link></li>
         <BarreRecherche></BarreRecherche>
-      </ul>
-    </nav>
-/* import Card_collection from '../card_collection/card_collection'
-export default function Navbar() {
-
-  const [error, setError] = useState(null);
-  const [categorie, setCategorie] = useState([]);
-  const [categorieId] = useState([]);
-  const [SousCategorie, setSousCategorie] = useState([]);
-  const [SouscategorieId] = useState([]);
-
-
-
-
-  useEffect(() => {
-    axios("https://localhost:8000/api/categories")
-      .then((response) => {
-        setCategorie(response.data["hydra:member"])
-        setError(null);
-      })
-      .catch(setError);
-
-      axios("https://localhost:8000/api/souscategories")
-      .then((res) => {
-        setSousCategorie(res.data["hydra:member"])
-        setError(null);
-      })
-      .catch(setError);
-
-  }, []);
-  if (error) return <p>An error occurred</p>
-
-  return (
-    <nav className="Navbar">
-    {categorie.map((item) => (
-      categorieId.push(item.id)
-    ))};
-    {SousCategorie.map((item) => (
-      SouscategorieId.push(item.id)
-    ))};
-      <ul className="liste">
-        <div className="divlogo1">
-          <Link to={"/"}><img src="/images/Image_Navbar/logo.png" className="logo1" alt="costume" /></Link>
-        </div>
-        <li className="items">Costume</li>
-        <li className="items">Chemise</li>
-        <li className="items" id="collection_btn">Collection <img className='fleche' src='/images/Image_Navbar/fleche-removebg-preview.png'></img><Card_collection /></li> 
-        <li><Link to={"/"}><img className="logo2" src="/images/Image_Navbar/ajouter-au-panier.png" alt="costume"/></Link></li>
-        <li><Link to={"/login"}><img className="logo2" src="/images/Image_Navbar/logoprofil.png" alt="costume"/></Link></li>
-    </ul>
       
-    </nav> */
- 
+      </ul>
+    : null}
+    </nav>
+    
   )
 }
--- front/src/component/card_collection/card_collection.js
+++ front/src/component/card_collection/card_collection.js
@@ -1,31 +1,65 @@
 import React from 'react'
-
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function Card_collection() {
+
+
+  const [error, setError] = useState(false);
+  const [categorie, setCategorie] = useState(null);
+  const [SousCategorie, setSousCategorie] = useState(null);
+
+
+
+
+
+  useEffect(() => {
+    axios("https://localhost:8000/api/categories")
+      .then((response) => {
+        setCategorie(response.data["hydra:member"])
+        setError(null);
+      })
+      .catch(setError);
+
+    axios("https://localhost:8000/api/souscategories")
+      .then((res) => {
+        setSousCategorie(res.data["hydra:member"])
+        setError(null);
+      })
+      .catch(setError);
+
+  }, []);
+  if (error) return <p>An error occurred</p>
+
   return (
+
     <div className='collection'>
+      {categorie ?
+        <div className='text-collection'>
+          <div className='Vetement'>
+            <h1><Link to={"/result/categorie/" + categorie[2].id}>Vêtements</Link></h1>
+            <ul className='Vetement_ul'>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[3].id}>Pantalon</Link></li>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[4].id}>Gilet</Link></li>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[5].id}>Veste</Link></li>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[6].id}>Pull</Link></li>
+            </ul>
+          </div>
+          <div className='Accessoire'>
+            <h1><Link to={"/result/categorie/" + categorie[3].id}>Accessoire</Link></h1>
+            <ul className='Accesoire_ul'>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[0].id}>Cravate</Link></li>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[1].id}>Chaussures</Link></li>
+              <li><Link to={"/result/souscategorie/" + SousCategorie[2].id}>Ceinture</Link></li>
 
-      <div className='text-collection'>
-
-        <div className='Vetement'>
-          <h1>Vêtements</h1>
-          <ul className='Vetement_ul'>
-            <li>Pantalon</li>
-            <li>Gilet</li>
-            <li>Veste</li>
-            <li>Pull</li>
-          </ul>
+            </ul>
+          </div>
         </div>
-        <div className='Accessoire'>
-          <h1>Accessoires</h1>
-          <ul className='Accesoire_ul'>
-            <li>Cravatte</li>
-            <li>Chaussures</li>
-            <li>Ceinture</li>
-
-          </ul>
-        </div>
-      </div>
+        : null}
     </div>
+
   )
 }
+
+
