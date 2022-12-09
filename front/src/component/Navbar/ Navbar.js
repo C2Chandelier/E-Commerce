9cd120e79fcc962d6800dc41a2fb326173336f04
C2Chandelier <charlelie.chandelier@epitech.eu>
2022-12-09 14:49:23 +0100

    pour sajjad

--- front/src/component/Navbar/ Navbar.js
+++ front/src/component/Navbar/ Navbar.js
@@ -9,7 +9,7 @@ import BarreRecherche from '../Barre/barre'
 export default function Navbar() {
 
   const [error, setError] = useState(null);
-  const [categorie, setCategorie] = useState([]);
+  const [categorie, setCategorie] = useState("");
   const [categorieId] = useState([]);
   const [SousCategorie, setSousCategorie] = useState([]);
   const [SouscategorieId] = useState([]);
@@ -21,6 +21,7 @@ export default function Navbar() {
     axios("https://localhost:8000/api/categories")
       .then((response) => {
         setCategorie(response.data["hydra:member"])
+       
         setError(null);
       })
       .catch(setError);
@@ -34,30 +35,31 @@ export default function Navbar() {
 
   }, []);
   if (error) return <p>An error occurred</p>
-
+  console.log(categorie)
   return (
     <nav className="Navbar">
-    {categorie.map((item) => (
+    {/* {categorie.map((item) => (
       categorieId.push(item.id)
-    ))};
+    ))}
     {SousCategorie.map((item) => (
       SouscategorieId.push(item.id)
-    ))};
+    ))} */}
+    
       <ul className="liste">
         <div className="divlogo1">
           <Link to={"/"}><img src="/images/Image_Navbar/logo.png" className="logo1" alt="costume" /></Link>
         </div>
-        <li><Link to={"/result/categorie/" + categorieId[0]}>Costumes</Link></li>
-        <li><Link to={"/result/categorie/" + categorieId[1]}>Chemises</Link></li>
-        <li><Link to={"/result/categorie/" + categorieId[2]}>Vêtements</Link></li>
+        {/* <li><Link to={"/result/categorie/" + categorie[0].id}>Costumes</Link></li>
+        <li><Link to={"/result/categorie/" + categorie[1].id}>Chemises</Link></li>
+        <li><Link to={"/result/categorie/" + categorie[2].id}>Vêtements</Link></li> */}
 
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[0]}>cravate</Link></li>
+        {/* <li><Link to={"/result/souscategorie/" + SouscategorieId[0]}>cravate</Link></li>
         <li><Link to={"/result/souscategorie/" + SouscategorieId[1]}>chaussures</Link></li>
         <li><Link to={"/result/souscategorie/" + SouscategorieId[2]}>ceinture</Link></li>
         <li><Link to={"/result/souscategorie/" + SouscategorieId[3]}>pantalon</Link></li>
         <li><Link to={"/result/souscategorie/" + SouscategorieId[4]}>gilet</Link></li>
         <li><Link to={"/result/souscategorie/" + SouscategorieId[5]}>Veste</Link></li>
-        <li><Link to={"/result/souscategorie/" + SouscategorieId[6]}>pull</Link></li>
+        <li><Link to={"/result/souscategorie/" + SouscategorieId[6]}>pull</Link></li> */}
 
         <li><Link to={"/"}><img className="logo2" src="/images/Image_Navbar/ajouter-au-panier.png" alt="costume" /></Link></li>
         <li><Link to={"/login"}><img className="logo2" src="/images/Image_Navbar/logoprofil.png" alt="costume" /></Link></li>
