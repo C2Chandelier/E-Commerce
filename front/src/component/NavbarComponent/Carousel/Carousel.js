71453199213d3bd2f9cc1ae51c73ae9c9c9026cc
sajjd <sajjadjafari081@gmail.com>
2022-12-14 12:01:25 +0100

    Add:Images/Carousel

--- front/src/component/NavbarComponent/Carousel/Carousel.js
+++ front/src/component/NavbarComponent/Carousel/Carousel.js
@@ -4,10 +4,10 @@ import { Carousel } from 'react-responsive-carousel';
 import React from 'react'
 
 function Slider() {
-  return (
-    <Carousel>
+  return ( 
+    <Carousel autoPlay infiniteLoop transitionTime={5} selectedItem>
     <div>
-        <img src="./images/Image_Carousel/Carousel.jpg" alt="costume"/>
+        <img src="./images/Image_Carousel/Carousel6.jpg" alt="costume" />
     </div>
     <div>
         <img src="./images/Image_Carousel/Carousel2.jpg" alt="costume"/>
@@ -15,8 +15,18 @@ function Slider() {
     <div>
         <img src="./images/Image_Carousel/Carousel3.jpg" alt="costume" />
     </div>
+    <div>
+        <img src="./images/Image_Carousel/Carousel4.jpg" alt="costume" />
+    </div>
+    <div>
+        <img src="./images/Image_Carousel/Carousel5.jpg" alt="costume" />
+    </div>
+    <div>
+        <img src="./images/Image_Carousel/Carousel7.jpg" alt="costume" />
+    </div>
+
     </Carousel>
   )
-}
-
+} 
+ 
 export default Slider
