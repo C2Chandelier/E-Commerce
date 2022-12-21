9d0264709b86b6801b6a07c5cae2c90d58471493
C2Chandelier <charlelie.chandelier@epitech.eu>
2022-12-21 17:36:07 +0100

    push mercredi soir

--- front/src/component/paniervisiteur/connect/connect.js
+++ front/src/component/paniervisiteur/connect/connect.js
@@ -2,28 +2,17 @@ import { Link } from "react-router-dom";
 import './connect.css'
 export default function Connect(){
     return (
-        
-        //<div className="containeurr">
-        //        <p>Vous n'etes pas connecté !</p>
-        //        <button>Passer au paiement sans me connecter</button>
-        //    <div className="groupe">
-        //        <Link  to={"/login"}>Se connecter</Link>
-        //        <Link  to={"/register"}>Pas encore de compte ?</Link>
-        //    </div>
-        //</div>
-        <div class="containerr">
+        <div className="containerr">
         <header>
             <h1>
-                <a href="#">
                     <img src={"http://tfgms.com/sandbox/dailyui/logo-1.png"} alt={"Authentic Collection"}/>
-                </a>
             </h1>
         </header>
-        <h1 class="text-center">Vous n'etes pas connecté !</h1>
-        <form class="registration-form">
+        <h1 className="text-center">Vous n'etes pas connecté !</h1>
+        <form className="registration-form">
             <div class="text-center">
-            <Link  to={"/register"}><button class="submit" name="register">S'inscrire</button></Link>
-            <Link  to={"/login"}><button class="submit" name="register"> Se Connecter</button></Link>
+            <Link  to={"/register"}><button className="submit" name="register">S'inscrire</button></Link>
+            <Link  to={"/login"}><button className="submit" name="register"> Se Connecter</button></Link>
             </div>
             <div className="text-centerSansCo">
                 <button className="sansconnection">Passer au paiement sans me connecter</button>
