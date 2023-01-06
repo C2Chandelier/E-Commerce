86baabda926d1671e250deefd968d3852ec8c096
C2Chandelier <charlelie.chandelier@epitech.eu>
2023-01-06 15:29:59 +0100

    savemodif 1er

--- front/src/component/profil/profil.js
+++ front/src/component/profil/profil.js
@@ -9,77 +9,136 @@ export default function Profil() {
     const navigate = useNavigate();
     const location = useLocation()
     const [users, setUsers] = useState([]);
-    const [modif,setModif] = useState(false);
+    const [modif, setModif] = useState(false);
+
+    const [nom, setNom] = useState("");
+    const [prenom, setPrenom] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [pays, setPays] = useState("");
+    const [Adresse, setAdresse] = useState("");
+    const [Tel, setTel] = useState("");
+    const [zipcode, setZipcode] = useState("");
+    const [Ville, setVille] = useState("");
+
 
 
     useEffect(() => {
         if (location.state !== "login") {
             navigate("/login", { state: { data: "profil" } })
         }
-        axios("https://localhost:8000/api/users/" + id_user)
-            .then((response) => {
-                console.log(response)
-                setUsers(response.data)
-            })
-        }, [])
-    function modifier(){
+        else {
+            axios("https://localhost:8000/api/users/" + id_user)
+                .then((response) => {
+                    setUsers(response.data)
+                })
+        }
+    }, [])
+
+    function modifier() {
         setModif(true);
     }
-/*     function savemodif(){
 
-    } */
-  
+    function savemodif() {
+        users.Nom = nom
+        users.Prenom = prenom
+        users.email = email
+        users.password = password
+        users.Pays = pays
+        users.Adresse = Adresse
+        users.Ville = Ville
+        users.Tel = Tel
+        users.zipcode = zipcode
+
+        const configuration = { headers: { 'Content-Type': "application/merge-patch+json", Accept: "application/ld+json" } }
+        axios.patch('https://localhost:8000/api/users/' + id_user,
+            {
+                email: users.email,
+                password: users.password,
+                role: users.role,
+                Nom: users.Nom,
+                Prenom: users.Prenom,
+                Tel: users.Tel,
+                Adresse: users.Adresse,
+
+            }
+            , configuration)
+    }
+
+    console.log(users)
+
     return (
+        users !== [] && users.length !== 0 ?
 
-        <div className="mainprofiluser">
-            <div className="titreedit">
-                <h3>Editer son profil</h3>
-            </div>
-            {modif === false ? 
-            <div className="profiluser">
-                <p>Nom : {users.Nom}</p>
-                <p>Prénom : {users.Prenom}</p>
-                <p>Email : {users.email}</p>
-                <p>Password : {users.password}</p>
-                <p>Pays : {users.Pays}</p>
-                <p>Adresse : {users.Adresse}</p>
-                <p>Téléphone : {users.Tel}</p>
-                <p>Code Postal : {users.zipcode}</p>
-                <div className="paiement">
-                    <p>Moyen de paiement enregistré : </p>
-                    <p>Carte finissant par {users.paiement.carte.substring(12)}</p> 
-                    <button>Modifier ce moyen de paiement</button>
+            <div className="mainprofiluser">
+                <div className="titreedit">
+                    <h3>Editer son profil</h3>
                 </div>
-                <button onClick={modifier}>modif</button>
-            </div>
-            
-            :
-            
-            <div className="profiluserchange">
-            <label>
-                Nom :
-                <input type="text" value={users.Nom}/>
-                Prénom :
-                <input type="text" value={users.Prenom}/>
-                Email :
-                <input Email="text" value={users.email} />
-                Password :
-                <input Password="text" value={users.password}/>
-                Pays :
-                <input Pays="text" value={users.Pays}/>
-                Adresse :
-                <input Adresse="text" value={users.Adresse}/>
-                Tel :
-                <input Téléphone="text" value={users.Tel}/>
-                Code Postal :
-                <input Code Postal="text" value= {users.zipcode}/> 
+                {modif === false ?
+                    <div className="profiluser">
+                        <p>Nom : {users.Nom}</p>
+                        <p>Prénom : {users.Prenom}</p>
+                        <p>Email : {users.email}</p>
+                        <p>Password : {users.password}</p>
+                        <p>Pays : {users.Pays}</p>
+                        <p>Adresse : {users.Adresse}</p>
+                        <p>Ville : {users.Ville}</p>
+                        <p>Téléphone : {users.Tel}</p>
+                        <p>Code Postal : {users.zipcode}</p>
+                        <button onClick={modifier}>modif</button>
+                        <div className="paiement">
+                            <p>Moyen de paiement enregistré : </p>
+                            <p>Carte finissant par {(users.paiement.carte).substring(12)}</p>
+                            <button>Modifier ce moyen de paiement</button>
+                        </div>
+                    </div>
 
-            </label>
-            {/* <button onclick={savemodif}>Enregistrer les modifications</button> */}
+                    :
+
+                    <div className="profiluserchange">
+                        <label>
+                            Nom :
+                            <input type="text" value={users.Nom} onChange={e => setNom(e.target.value)} />
+                        </label>
+                        <label>
+                            Prénom :
+                            <input type="text" value={users.Prenom} onChange={e => setPrenom(e.target.value)} />
+                        </label>
+                        <label>
+                            Email :
+                            <input Email="text" value={users.email} onChange={e => setEmail(e.target.value)} />
+                        </label>
+                        <label>
+                            Password :
+                            <input Password="text" value={users.password} onChange={e => setPassword(e.target.value)} />
+                        </label>
+                        <label>
+                            Pays :
+                            <input Pays="text" value={users.Pays} onChange={e => setPays(e.target.value)} />
+                        </label>
+                        <label>
+                            Ville :
+                            <input Pays="text" value={users.Ville} onChange={e => setVille(e.target.value)} />
+                        </label>
+                        <label>
+                            Adresse :
+                            <input Adresse="text" value={users.Adresse} onChange={e => setAdresse(e.target.value)} />
+                        </label>
+                        <label>
+                            Tel :
+                            <input Téléphone="text" value={users.Tel} onChange={e => setTel(e.target.value)} />
+                        </label>
+                        <label>
+                            Code Postal :
+                            <input Code Postal="text" value={users.zipcode} onChange={e => setZipcode(e.target.value)} />
+                        </label>
+                        <button onClick={savemodif}>Enregistrer les modifications</button>
+                    </div>
+                }
             </div>
-        }
-    </div>
+
+            : null
 
     )
-} 
+}
 
